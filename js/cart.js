aa26b28928aba94193fb45118d7826667f76a879
BackYard911 <yousseftarek9801@gmail.com>
2021-06-23 04:15:29 +0200

    shoppingCart adjustments

--- js/cart.js
+++ js/cart.js
@@ -7,7 +7,7 @@ const BASE_URL = "http://localhost:8000/api/products/";
   if (cartProducts) {
     cartProducts.forEach(async (pId) => {
       /// get product
-      const response = await fetch(GET_PRODUCTS + pId, {
+      const response = await fetch(BASE_URL + pId, {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
@@ -17,20 +17,25 @@ const BASE_URL = "http://localhost:8000/api/products/";
       });
       const product = await response.json();
       const productElm = document.createElement("div");
-      productElm.className = "item";
-      productElm.setAttribute("product-id", product.id);
+      // productElm.className = "row";
+      // productElm.setAttribute("product-id", product.id);
       productElm.innerHTML = `
+          <div class="item" product-id="${product.id} col-sm-4">
           <img src=${product.product_images[0].url} class="itemImage" />
-                  <p class="productName">${product.title}</p>
-                  <p class="itemDetails">
-                    ${product.description}
-                  </p>
-                  <div class="flex">
-                    <p class="price">${product.price}</p>
-                    <input type="number" class="quantity" min="0" value="1" />
-                  </div>
+          <p class="productName">${product.title}</p>
+          <p class="itemDetails">
+            ${product.description}
+          </p>
+          <div class="flex">
+            <p class="price">${product.price} egp</p>
+            <input type="number" class="quantity" min="0" value="1" />
+          </div>
+          </div>
           `;
       cartProductsContainer.appendChild(productElm);
+      const total = calculateCost();
+      document.querySelector(".totalCost").innerText = `Total: ${total} EGP`;
+      updateTotal();
     });
   }
 })();
@@ -39,10 +44,10 @@ const orderBtn = document.querySelector(".orderButton");
 
 orderBtn.addEventListener("click", async (e) => {
   e.preventDefault();
-  const total = document.querySelector(".totalCost").split(" ")[1];
+  const total = document.querySelector(".totalCost").innerText.split(" ")[1];
   //   const userId = loggedUser.user.id;
 
-  const productsElms = document.querySelector(".items");
+  const productsElms = document.querySelectorAll(".item");
   const order_products = [];
   productsElms.forEach((productsElm) => {
     const quantity = productsElm.querySelector(".quantity").value;
@@ -57,16 +62,52 @@ orderBtn.addEventListener("click", async (e) => {
     order_products,
   };
   const POST_ORDER = "http://localhost:8000/api/orders";
-  const response = await fetch(BASE_URL, {
+  console.log(order);
+  const response = await fetch(POST_ORDER, {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
+      Authorization: `Bearer ${loggedUser.token}`,
     },
     method: "POST",
     body: JSON.stringify(order),
   });
   const result = await response.json();
   if (result.message != "failed") {
+    alert("Order has been placed");
+    localStorage.removeItem("cart");
     location.replace("./index.html");
+        
   }
+
 });
+
+function calculateCost(){
+  let total = 0;
+  const productsElms = document.querySelectorAll(".item");
+  productsElms.forEach(function(productElm){
+    const quantity = parseInt(productElm.querySelector(".quantity").value);
+    const price = Number(productElm.querySelector(".price").innerText.split(" ")[0]);
+    
+    total += quantity * price;
+  })
+  return total.toFixed(2);
+}
+
+function updateTotal(){
+
+  const quantitites = document.querySelectorAll(".quantity");
+  console.log(quantitites);
+  
+  for (let i = 0; i<quantitites.length;i++){
+    quantitites[i].addEventListener("mouseup",function(){
+      const total = calculateCost();
+      document.querySelector(".totalCost").innerText = `Total: ${total} EGP`;
+    })
+    quantitites[i].addEventListener("keyup",function(){
+      const total = calculateCost();
+      document.querySelector(".totalCost").innerText = `Total: ${total} EGP`;
+    })
+  }
+}
+
